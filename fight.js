b405898552db5c544f4675b214ecfae4108819c0
kgolobok <kgolobok@umich.edu>
2016-04-04 20:53:56 -0400

    Fight.js Implementation
    Implementation for attackCharacter() and runRound()

--- fight.js
+++ fight.js
@@ -10,8 +10,22 @@ class Character {
   }
 }
 
+//REQUIRES: defender is a vlaid character
+//MODIFIES: defender
+//EFFECTS:  Carries out the attacks in accordance with the spec
 Character.prototype.attackCharacter = function(defender) {
-  // Implement me!
+  var base_dmg = this.attack - defender.defense;
+  //times 6 because we use floor function for even distribution of probabilities
+  var rand_dmg = Math.floor(Math.random()*6);
+  var tot_dmg = base_dmg + rand_dmg;
+  
+  //Kills or damages the defender
+  if(tot_dmg >= defender.health)
+    defender.health = 0;
+  else
+    defender.health -= tot_dmg;
+  
+  console.log(this.name + " does " + tot_dmg + " damage to " + defender.name); 
 }
 
 // Main Fight Logic
@@ -25,8 +39,28 @@ while (player.health && enemy.health) {
   console.log('');
 }
 
+//REQUIERS: p1, p2 are valid characters
+//MODIFIES: p1, p2
+//EFFECTS:  Plays a round of the fight
 function runRound(round, p1, p2) {
-  // Implement me!
+  console.log("----- Round " + round + " -----");
+
+  //perform attacks
+  p1.attackCharacter(p2);
+  if(p2.health <= 0){
+    endGame(p1, p2); //p2 lost
+    return;
+  }
+
+  p2.attackCharacter(p1);
+  if(p1.health <= 0){
+    endGame(p2, p1); //p1 lost
+    return;
+  }
+
+  //print health
+  console.log(p1.name + " health: " + p1.health);
+  console.log(p2.name + " health: " + p2.health);
 }
 
 function endGame(winner, loser) {
